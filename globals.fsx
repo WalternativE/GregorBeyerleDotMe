715797941642103ee4fc43272f5a0eaee5142648
WalternativE <gregor.beyerle@gmail.com>
2020-11-30 20:38:56 +0100

    Adds fix for missing file extension in blog posts

--- globals.fsx
+++ globals.fsx
@@ -7,25 +7,38 @@ type Utf8StringWriter() =
 
   override this.Encoding = Text.Encoding.UTF8
 
-let toPostLink (fileName: string) =
+let extractUrlContent (fileName: string) =
   let regex = """(\d{4}-\d{2}-\d{2})-(.*).md"""
   let ``match`` = Regex.Match(fileName, regex)
 
-  if ``match``.Success then
-    let datePart =
-      ``match``.Groups.[1].Value
-      |> fun p -> p.Split('-') |> List.ofArray
-      |> function
-      | year :: month :: [ day ] -> sprintf "%s/%s/%s" year month day
-      | _ -> failwith "The world just exploded"
+  let result =
+    if ``match``.Success then
+        let datePart =
+          ``match``.Groups.[1].Value
+          |> fun p -> p.Split('-') |> List.ofArray
+          |> function
+          | year :: month :: [ day ] -> sprintf "%s/%s/%s" year month day
+          | _ -> failwith "The world just exploded"
 
+        (datePart, ``match``.Groups.[2].Value)
+    else
+      sprintf "The post filename %s was not in the correct format!" fileName
+      |> failwith
+
+  result
+
+let toPostLink (fileName: string) =
 #if WATCH
-    let suffix = ".html"
+  let suffix = ".html"
 #else
-    let suffix = String.Empty
+  let suffix = String.Empty
 #endif
 
-    sprintf "/posts/%s/%s%s" datePart ``match``.Groups.[2].Value suffix
-  else
-    sprintf "The post filename %s was not in the correct format!" fileName
-    |> failwith
+  let (datePart, postName) = extractUrlContent fileName
+
+  sprintf "/posts/%s/%s%s" datePart postName suffix
+
+let toHtmlFileName (fileName: string) =
+  let (datePart, postName) = extractUrlContent fileName
+
+  sprintf "/posts/%s/%s%s" datePart postName ".html"
